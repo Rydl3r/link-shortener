d72b2701921dc173544697f914afa178c66a6142
Ivan Mukoed <1rydler@gmail.com>
2022-06-14 17:31:00 +0300

    syntax fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Shortener from "./components/Shortener";
 
-
 function App() {
   return (
     <div className="App">
--- src/components/Shortener.tsx
+++ src/components/Shortener.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import { Button, Form, FormGroup, Alert, Input } from 'reactstrap';
-import styled from 'styled-components'
-import { AiOutlineCopy } from 'react-icons/ai';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { validURL } from '../utils/checkURL'
+import { useState } from "react";
+import { Button, Form, FormGroup, Alert, Input } from "reactstrap";
+import styled from "styled-components";
+import { AiOutlineCopy } from "react-icons/ai";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { validURL } from "../utils/checkURL";
 
 const Shortener = () => {
-
-  const [link, setLink] = useState("")
-  const [result, setResult] = useState("")
+  const [link, setLink] = useState("");
+  const [result, setResult] = useState("");
 
   const StyledAlert = styled(Alert)`
     width: 400px;
@@ -23,58 +22,82 @@ const Shortener = () => {
   `;
 
   const shortenLink = async () => {
-    console.log(link)
+    console.log(link);
     if (!validURL(link)) {
-      alert("Invalid URL!")
-      setLink("")
+      alert("Invalid URL!");
+      setLink("");
     } else {
-      await fetch('https://api-ssl.bitly.com/v4/shorten', {
-        method: 'POST',
+      await fetch("https://api-ssl.bitly.com/v4/shorten", {
+        method: "POST",
         headers: {
-          'Authorization': 'Bearer 12824ec323d6ad462f300b661821127e064fecc0',
-          'Content-Type': 'application/json'
+          Authorization: "Bearer 12824ec323d6ad462f300b661821127e064fecc0",
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ "long_url": link, "domain": "bit.ly", "group_guid": "Bl9hb7gDsWp" })
+        body: JSON.stringify({
+          long_url: link,
+          domain: "bit.ly",
+          group_guid: "Bl9hb7gDsWp",
+        }),
       })
         .then((res) => res.json())
         .then((data) => setResult(data.id));
     }
-
-  }
+  };
 
   return (
     <div>
       <h1 className="d-flex justify-content-center">Your link shortener</h1>
       <Form className="d-flex justify-content-center pt-3">
         <FormGroup className="mr-2">
-          <Input value={link} onChange={(e) => {
-            let newLink: string = (e.target.value.indexOf('://') === -1) ? 'http://' + e.target.value : e.target.value
-            setLink(newLink)
-          }} type="text" name="link" id="linkInput" placeholder="Input your link" />
+          <Input
+            value={link}
+            onChange={(e) => {
+              let newLink: string =
+                e.target.value.indexOf("://") === -1
+                  ? "http://" + e.target.value
+                  : e.target.value;
+              setLink(newLink);
+            }}
+            type="text"
+            name="link"
+            id="linkInput"
+            placeholder="Input your link"
+          />
         </FormGroup>
-        <Button className="ml-2" color="primary" onClick={() => {
-          shortenLink()
-        }}>Shorten!</Button>
+        <Button
+          className="ml-2"
+          color="primary"
+          onClick={() => {
+            shortenLink();
+          }}
+        >
+          Shorten!
+        </Button>
       </Form>
       <div>
-        {result === ""
-          ?
+        {result === "" ? (
           ""
-          :
-          <StyledAlert className="d-flex justify-content-center" color="primary">
+        ) : (
+          <StyledAlert
+            className="d-flex justify-content-center"
+            color="primary"
+          >
             <div className="mr-3">Your link - {result}</div>
             <CopyToClipboard text={result}>
-              <StyledButton className="ml-3" onClick={() => {
-                alert('Copied!')
-              }}>
+              <StyledButton
+                className="ml-3"
+                onClick={() => {
+                  alert("Copied!");
+                }}
+              >
                 <AiOutlineCopy />
               </StyledButton>
             </CopyToClipboard>
           </StyledAlert>
-        }
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Shortener
+export default Shortener;
